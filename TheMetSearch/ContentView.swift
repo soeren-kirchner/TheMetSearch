61385d9b2fd7f3e2c666a4314e365e19e7784505
Sören Kirchner <sk@tologo.de>
2022-08-29 19:33:46 +0200

    Initial Commit

---
+++ TheMetSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMetSearch
+//
+//  Created by Sören Kirchner on 29.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMetSearch/TheMetSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetSearchApp.swift
+//  TheMetSearch
+//
+//  Created by Sören Kirchner on 29.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TheMetSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
