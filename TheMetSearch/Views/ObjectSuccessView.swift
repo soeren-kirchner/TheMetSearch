d076bf282188b7e269551f023fbd5828b739b122
Sören Kirchner <sk@tologo.de>
2022-09-04 17:52:30 +0200

    implement ObjectSuccessView

--- TheMetSearch/Views/ObjectSuccessView.swift
+++ TheMetSearch/Views/ObjectSuccessView.swift
@@ -13,24 +13,74 @@ struct ObjectSuccessView: View {
     
     var body: some View {
         ScrollView {
-            VStack {
-    //            if let url = URL(string: object.primaryImage) {
-    //                AsyncImage(url: url)
-    //                    .frame(height: 400)
-    //                    .clipped()
-    //            }
-                if let url = URL(string: object.primaryImage) {
-                    PrimaryImageView(url: url)
-                }
-                Text(object.primaryImage)
-                Text(object.title)
+            VStack(alignment: .leading, spacing: 30) {
+                headerImage
+                objectHeader
+                objectDetails
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    var headerImage: some View {
+        HStack {
+            Spacer()
+            if let url = URL(string: object.primaryImageSmall) {
+                PrimaryImageView(url: url)
+            }
+            Spacer()
+        }
+    }
+    
+    var objectHeader: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(object.title)
+                .font(.system(size: 24))
+                .fontWeight(.bold)
+            Text(object.artistDisplayName)
+                .font(.system(size: 20))
+                .fontWeight(.medium)
+        }
+    }
+    
+    var objectDetails: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            ForEach(cleanedDetails, id: \.self) { labelFieldPair in
+                line(labelFieldPair[0], labelFieldPair[1])
             }
         }
     }
+    
+    func line(_ label: String, _ content: String) -> some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.system(size: 14))
+                .fontWeight(.light)
+                .textCase(.uppercase)
+            Text(content)
+                .font(.system(size: 18))
+                .fontWeight(.medium)
+                .lineLimit(2)
+        }
+    }
+
+    var cleanedDetails: [[String]] {
+        return [
+            ["Object Name", object.objectName],
+            ["Culture", object.culture],
+            ["Period", object.period],
+            ["Medium", object.medium],
+            ["Dimensions", object.dimensions]
+        ].filter{ !$0[1].isEmpty }
+    }
 }
 
-//struct ObjectSuccessView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ObjectSuccessView()
-//    }
-//}
+struct ObjectSuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ObjectSuccessView(object: .example2)
+            ObjectSuccessView(object: .example)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
