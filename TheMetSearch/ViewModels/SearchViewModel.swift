8fd24b2b1910297a716febccfa880fd2576a1559
Sören Kirchner <sk@tologo.de>
2022-09-05 23:48:51 +0200

    change character count test and Task cancelling into the right order. move url test to the API

--- TheMetSearch/ViewModels/SearchViewModel.swift
+++ TheMetSearch/ViewModels/SearchViewModel.swift
@@ -11,43 +11,37 @@ class SearchViewModel: ObservableObject {
     
     @Published var objects = Objects(total: 0, objectIDs: [])
     @Published var searchText = ""
-//    @Published var isSearching = false
     @Published var searchState: SearchState = .toFewCharacters
     
     private var searchTask: Task<(), Never>?
     
     func search() {
-        if searchText.count < 3 {
+        print("search called for \(searchText)")
+        searchTask?.cancel()
+        if searchText.count < TheMetDefaults.minimumInputCharachters {
             searchState = .toFewCharacters
             return
         }
-        print("search called")
-        print(searchText)
-        searchTask?.cancel()
         searchState = .searching
         searchTask = Task {
             await fetch(for: searchText)
         }
         print("leave search")
     }
-//    func search(for keyword: String, withImagesOnly: Bool = false) async {
-//        searchTask?.cancel()
-//    }
-    
+
     private func fetch(for keyword: String) async {
-        guard let url = API.search(for: keyword) else { return }
-        let result = await Client.fetchData(for: URLRequest(url: url), of: Objects.self)
+        let result = await API.fetchObjects(for: keyword)
         if Task.isCancelled {
             print("Task canceled")
             return
         }
-        DispatchQueue.main.async {
+        await MainActor.run {
             switch result {
             case .success(let objects):
                 self.searchState = .success
                 self.objects = objects
-            case .failure(_):
-                self.searchState = .error
+            case .failure(let error):
+                self.searchState = .error(error)
             }
         }
     }
@@ -57,5 +51,5 @@ enum SearchState {
     case toFewCharacters
     case searching
     case success
-    case error
+    case error(APIError)
 }
